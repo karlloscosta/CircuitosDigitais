45499dd5f5402e6dc170aa685c519d4d53a963b8
Paulo H. <paulorn09@gmail.com>
2024-08-28 19:59:33 -0300

    correções

--- Lab_de_Circuitos/Display_7segmentos/display.sv
+++ Lab_de_Circuitos/Display_7segmentos/display.sv
@@ -3,10 +3,10 @@ module display (
     output logic a, b, c, d, e, f, g
 );
     assign a = ~i1 & ~i2 & ~i3 & i4 | ~i1 & i2 & ~i3 & ~i4 | i1 & ~i2 & i3 & i4 | i1 & i2 & ~i3 & i4;
-    assign b = ~i1 & i2 & ~i3 & i4 | i2 & i3 & ~i4 | i1 & i3 & ~i4 | i1 & i3 & i4;
-    assign c = ~i1 & ~i2 & i3 & ~i4 | i1 & i2 & i3 | i1 & i2 & i3;
+    assign b = ~i1 & i2 & ~i3 & i4 | i2 & i3 & ~i4 | i1 & i2 & ~i4 | i1 & i3 & i4;
+    assign c = ~i1 & ~i2 & i3 & ~i4 | i1 & i2 & i4 | i1 & i2 & i3;
     assign d = ~i1 & ~i2 & ~i3 & i4 | ~i1 & i2 & ~i3 & ~i4 | i1 & ~i2 & i3 & ~i4 | i2 & i3 & i4;
     assign e = ~i2 & ~i3 & i4 | ~i1 & i2 & ~i3 | ~i1 & i4;
-    assign f = i1 & i2 & ~i3 & i4 | ~i1 & ~i2 & i4 | ~i1 & ~i2 & i3 | ~i1 & i2 & i4;
+    assign f = i1 & i2 & ~i3 & i4 | ~i1 & ~i2 & i4 | ~i1 & ~i2 & i3 | ~i1 & i3 & i4;
     assign g = i1 & i2 & ~i3 & ~i4 | ~i1 & i2 & i3 & i4 | ~i1 & ~i2 & ~i3;
 endmodule
